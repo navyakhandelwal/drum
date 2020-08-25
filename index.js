9c07d6cef54d8423f94eb6745b2b9082e7cfb7bf
Navya Khandelwal <68371782+navyakhandelwal@users.noreply.github.com>
2020-08-25 15:20:59 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+var i;
+for(i=0;i<=6;i++)
+{
+document.querySelectorAll(".drum")[i].addEventListener("click",aud);
+function aud()
+{
+  this.style.color="white";
+  var nav=this.innerHTML;
+  switch (nav) {
+    case 'w':
+    var audio=new Audio("sounds/tom-1.mp3");
+    audio.play();
+      break;
+      case 'a':
+      var audio=new Audio("sounds/tom-2.mp3");
+      audio.play();
+        break;
+        case 's':
+        var audio=new Audio("sounds/tom-3.mp3");
+        audio.play();
+          break;
+          case 'd':
+          var audio=new Audio("sounds/tom-4.mp3");
+          audio.play();
+            break;
+            case 'j':
+            var audio=new Audio("sounds/snare.mp3");
+            audio.play();
+              break;
+            case 'k':
+            var audio=new Audio("sounds/crash.mp3");
+            audio.play();
+              break;
+              case 'l':
+              var audio=new Audio("sounds/kick-bass.mp3");
+              audio.play();
+                break;
+
+    default:console.log("nav");
+
+  }
+}
+}
